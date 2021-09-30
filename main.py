e6e97373e61abacd09c9368fdabc23c18ba59f88
NabarunKar <nabarunkar01@gmail.com>
2021-10-01 00:17:22 +0530

    scores

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 screen = Screen()
 
@@ -13,7 +14,7 @@ screen.tracer(0)
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
-
+scoreboard = Scoreboard()
 screen.tracer(0)  # ignore animations
 
 
@@ -41,9 +42,13 @@ while game_is_on:
 
     # paddle miss
 
+    # right miss
     if ball.xcor() >= 380:
         ball.reset_pos()
+        scoreboard.l_point()
 
+    # left miss
     if ball.xcor() <= -380:
         ball.reset_pos()
+        scoreboard.r_point()
 screen.exitonclick()
